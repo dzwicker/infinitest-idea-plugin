dee5311d97a8c23ca64ccde1f2b17bb79b353d3a
dzwicker <daniel.zwicker@in2experience.com>
2012-11-22 09:49:51 +0100

    setup intellij plugin project

---
+++ src/main/java/com/in2experience/infinitest/idea/window/InfinitestToolWindowCondition.java
@@ -0,0 +1,11 @@
+package com.in2experience.infinitest.idea.window;
+
+import com.intellij.openapi.util.Condition;
+
+public class InfinitestToolWindowCondition implements Condition {
+
+	@Override
+	public boolean value(final Object o) {
+		return true;
+	}
+}
---
+++ src/main/java/com/in2experience/infinitest/idea/window/InfinitestToolWindowFactory.java
@@ -0,0 +1,12 @@
+package com.in2experience.infinitest.idea.window;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+
+public class InfinitestToolWindowFactory implements ToolWindowFactory {
+
+	@Override
+	public void createToolWindowContent(final Project project, final ToolWindow toolWindow) {
+	}
+}
